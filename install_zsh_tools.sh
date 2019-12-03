a0e40ce2bb09aa4f4dba1f2f89d73eb3d15a67c3
Peer Herrmann <mail@peerherrmann.de>
2019-12-03 14:16:29 +0100

    changed install script

--- install_zsh_tools.sh
+++ install_zsh_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' \~/.zshrc
-sed -i '.bak' 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' \~/.zshrc
+sed -i '.bak' 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
+sed -i '.bak' 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
